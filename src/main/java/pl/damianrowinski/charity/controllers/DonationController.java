1bb5a1d936b0b3ded79c4212e9cb0ce8a3034366
Damian Rowiński <damian.rowinski@gmail.com>
2020-10-20 20:36:50 +0200

    added Donation Controller form Get and Post methods

--- src/main/java/pl/damianrowinski/charity/controllers/DonationController.java
+++ src/main/java/pl/damianrowinski/charity/controllers/DonationController.java
@@ -1,18 +1,27 @@
 package pl.damianrowinski.charity.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.damianrowinski.charity.domain.entities.Donation;
+import pl.damianrowinski.charity.services.DonationService;
 
 @Controller
-
+@RequiredArgsConstructor
 public class DonationController {
+    private final DonationService donationService;
 
     @GetMapping("/donation/form")
     public String generateForm(Model model) {
-        Donation donation = new Donation();
-        model.addAttribute("donation", donation);
+        model.addAttribute("donation", new Donation());
         return "donation_form";
+    }
+
+    @PostMapping("/donation/form")
+    public String sendForm(Donation donation) {
+        donationService.add(donation);
+        return "redirect:/";
     }
 }
